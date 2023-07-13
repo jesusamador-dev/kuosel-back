77b7bcdd197dce53023ad8b4844d3c6cc14b5707
Jesus Macias <jesus.macias.amador@gmail.com>
2023-07-13 09:55:09 -0600

    fix: quitanto un error

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routers import categories, movements
 
-app = FastAPI(prefix)
+app = FastAPI()
 
 app.include_router(categories.router)
 app.include_router(movements.router)
