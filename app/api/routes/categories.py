2ddf3d6eca817661f10e7ce5eeb5efa41e18de8c
Jesus Macias <jesus.macias.amador@gmail.com>
2023-07-24 21:03:26 -0600

    fix: se soluciona el error de response model

--- app/api/routes/categories.py
+++ app/api/routes/categories.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 from app.domain.services.category_service import CategoryService
+from app.schemas.category_schema import Category
 
 router = APIRouter()
 
+service = CategoryService()
 
-@router.get("/categories")
-async def get_categories(service: CategoryService):
-    return await service.get_all()
+
+@router.get("/categories", response_model=list[Category])
+async def get_categories():
+    return service.get_all()
--- app/domain/services/category_service.py
+++ app/domain/services/category_service.py
@@ -1,12 +1,12 @@
 from fastapi import Depends
 from app.infra.repositories.category_repository import CategoryRepository
-from app.domain.models.Category import Category
+from app.schemas.category_schema import Category
 
 
 class CategoryService:
     def __init__(self, category_repo: CategoryRepository = Depends(CategoryRepository)):
         self.category_repo = category_repo
 
-    def get_all(self):
+    def get_all(self) -> list[Category]:
         return self.category_repo.get_all()
 
